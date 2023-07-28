4b081dbccdf7c80218c387e145a15e5f9b7620ee
Alex <a1980dominov@gmail.com>
2023-07-28 22:08:31 +0300

    Add project files.

---
+++ emptyProject_task/Models/TaskDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace emptyProject_task.Models
+{
+    public class TaskDbContext:DbContext
+    {
+        public DbSet<Task> Tasks { get; set;} //Dbset
+        public TaskDbContext(DbContextOptions options) : base(options) { Database.EnsureCreated(); }
+        
+
+    }
+}
